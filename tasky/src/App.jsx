9c77c37c22eb606e744b49e069eb11ca271fc346
(20098855) Zacheriah Ibekwe <20098855@setu.ie>
2026-01-28 16:48:52 +0000

    Completed Tasky Lab 1

---
+++ tasky/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css';
+import Task from './components/Task';
+
+function App() {
+  return (
+    <div className="container">
+
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today" >
+     Empty Dishwasher
+    </Task>
+      <Task title="Laundry" deadline="Tomorrow" >
+         Fold laundry and put away
+    </Task>
+      
+    
+      <Task title="Tidy" deadline="Today" />
+   
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ tasky/src/components/Task.jsx
@@ -0,0 +1,15 @@
+const Task = (props) => {
+    
+    return (
+           <div className="card">
+             <p className="title">{props.title}</p>
+            <p>Due: {props.deadline}</p>
+            <p>{props.children}</p>
+        </div>
+    )
+
+    
+}
+
+
+export default Task;
