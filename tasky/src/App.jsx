e085c0ae29c210b870b4ab1dca47c1a449738678
Your <youremail@anyemailwilldo.com>
2026-02-04 19:31:58 +0000

    Added task state

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,17 +1,24 @@
 import './App.css';
 import Task from './components/Task';
+import React, { useState } from 'react';
 
 function App() {
+    const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { title: "Tidy up", deadline: "Today" }
+    ]
+  });
+
   return (
         <div className="container">
 
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" >
-        Empty Dishwasher</Task>
-       <Task title="Laundry" deadline="Tomorrow">
-        Fold laundry and put away
-    </Task>
-      <Task title="Tidy" deadline="Today" />
+           <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
+      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
+      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
+
     </div>
   );
 }
