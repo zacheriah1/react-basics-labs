c07552f89289dc3590ff3c9044829496f57a1097
Your <youremail@anyemailwilldo.com>
2026-02-09 18:32:30 +0000

    Added keys to tasks

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -3,7 +3,7 @@ import Task from './components/Task';
 import React, { useState } from 'react';
 
 function App() {
- const [ taskState, setTaskState ] = useState({
+   const [ taskState, setTaskState ] = useState({
     tasks: [
       { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
       { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
@@ -11,21 +11,22 @@ function App() {
     ]
   });
 
-  return (
-        <div className="container">
 
+    return (
+    <div className="container">
       <h1>Tasky</h1>
-  {taskState.tasks.map((task) => (              
+       {taskState.tasks.map((task) => (              
     <Task 
-      title={task.title}
+     title={task.title}
       description={task.description}
       deadline={task.deadline}
       key={task.id}
     />
-  ))}
- 
+  ))} 
+
     </div>
   );
+
 }
 
 export default App;
