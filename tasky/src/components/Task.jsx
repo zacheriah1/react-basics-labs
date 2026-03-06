946b1579fa6bac698291049392791bdf89c1b41f
(20098855) Zacheriah Ibekwe <20098855@setu.ie>
2026-03-06 17:29:43 +0000

    Completed Tasky Lab 1

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -1,14 +1,11 @@
 const Task = (props) => {
-    
-    return (
-        <div className="card">
-            <p className="title">{props.title}</p>
-            <p>Due: {props.deadline}</p>
-            <p>{props.children}</p>
-        </div>
-    )
-    
+  return (
+    <div className="card">
+      <p className="title">{props.title}</p>
+      <p>Due: {props.deadline}</p>
+      <p>{props.description}</p>
+    </div>
+  );
 }
-
 
 export default Task;
